4950892a40ea0d59cc8c0c6a491541e46db24611
ivan_pilapil <akosi.iban@gmail.com>
2017-08-25 18:17:05 +0800

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "creating 10 records for shorten_url table"
+
+10.times do |i|
+  ShortenUrl.create_record("http://google.com/#{SecureRandom.hex(10)}")
+end
+
+puts "creating some records for url_statistic table"
+
+ShortenUrl.all.each do |url|
+  5.times do |i|
+    UrlStatistic.create!(
+      ip_address: "127.0.0.#{rand(1..5)}",
+      shorten_url_id: url.id,
+      created_at: rand(5.days).seconds.ago
+    )
+  end
+
+  10.times do |i|
+    UrlStatistic.create!(
+      ip_address: "127.0.0.#{rand(1..3)}",
+      shorten_url_id: url.id,
+      created_at: rand(10.hours).seconds.ago
+    )
+  end
+end
+
+puts "done"
+
+
